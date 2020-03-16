c4cb28561b370e659966c6bef5a90364923601ea
Lou Shaw Yeong <shawyeong94@gmail.com>
2020-03-16 20:25:49 +0800

    finish SortMerge

--- src/qp/operators/SortMerge.java
+++ src/qp/operators/SortMerge.java
@@ -1,6 +1,7 @@
 /**
- * Page Nested Join algorithm
+ * Sort Merge join algorithm
  **/
+
 
 package qp.operators;
 
@@ -11,43 +12,30 @@ import qp.utils.Tuple;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
-public class SortMerge extends Join {
+ public class SortMerge extends Join {
+    private ExternalSort leftSort;
+    private ExternalSort rightSort;
 
-    static int filenum = 0;         // To get unique filenum for this operation
-    int batchsize;                  // Number of tuples per out batch
-    ArrayList<Integer> leftindex;   // Indices of the join attributes in left table
-    ArrayList<Integer> rightindex;  // Indices of the join attributes in right table
-    String rfname;                  // The file name where the right table is materialized
-    Batch outbatch;                 // Buffer page for output
-    Batch leftbatch;                // Buffer page for left input stream
-    Batch rightbatch;               // Buffer page for right input stream
-    ObjectInputStream in;           // File pointer to the right hand materialized file
+    private int leftJoinAttrIdx;
+    private int rightJoinAttrIdx;
+    private int batchNum;
+    
+    private ArrayList<Integer> leftindex;   // Indices of the join attributes in left table
+    private ArrayList<Integer> rightindex;  // Indices of the join attributes in right table
 
-    int lcurs;                      // Cursor for left side buffer
-    int rcurs;                      // Cursor for right side buffer
-    boolean eosl;                   // Whether end of stream (left table) is reached
-    boolean eosr;                   // Whether end of stream (right table) is reached
-
-    public SortMerge(Join jn) {
-        super(jn.getLeft(), jn.getRight(), jn.getConditionList(), jn.getOpType());
-        schema = jn.getSchema();
-        jointype = jn.getJoinType();
-        numBuff = jn.getNumBuff();
+    public SortMerge(Join join) {
+        super(join.getLeft(), join.getRight(), join.getCondition(), join.getOpType());
+        schema = join.getSchema();
+        jointype = join.getJoinType();
+        numBuff = join.getNumBuff();
     }
 
-    /**
-     * During open finds the index of the join attributes
-     * * Materializes the right hand side into a file
-     * * Opens the connections
-     **/
+    @Override
     public boolean open() {
-        /** select number of tuples per batch **/
-    	String hello = "hello";
-        int tuplesize = schema.getTupleSize();
-        batchsize = Batch.getPageSize() / tuplesize;
-
-        /** find indices attributes of join conditions **/
         leftindex = new ArrayList<>();
         rightindex = new ArrayList<>();
         for (Condition con : conditionList) {
@@ -56,137 +44,51 @@ public class SortMerge extends Join {
             leftindex.add(left.getSchema().indexOf(leftattr));
             rightindex.add(right.getSchema().indexOf(rightattr));
         }
-        Batch rightpage;
 
-        /** initialize the cursors of input buffers **/
-        lcurs = 0;
-        rcurs = 0;
-        eosl = false;
-        /** because right stream is to be repetitively scanned
-         ** if it reached end, we have to start new scan
-         **/
-        eosr = true;
+        // Find the batch size
+        int tupleSize = getSchema().getTupleSize();
+        batchNum = Batch.getPageSize() / tupleSize;
 
-        /** Right hand side table is to be materialized
-         ** for the Nested join to perform
-         **/
-        if (!right.open()) {
+        // Find the index of join attribute of in each relation
+        leftJoinAttrIdx = getLeft().getSchema().indexOf(getCondition().getLhs());
+        rightJoinAttrIdx = getRight().getSchema().indexOf((Attribute) getCondition().getRhs());
+
+        // Sort the 2 relations
+        leftSort = new ExternalSort(left, leftindex, numBuff);
+        rightSort = new ExternalSort(right, rightindex, numBuff);
+
+        if (!(leftSort.open() && rightSort.open())) {
             return false;
         } else {
-            /** If the right operator is not a base table then
-             ** Materialize the intermediate result from right
-             ** into a file
-             **/
-            filenum++;
-            rfname = "NJtemp-" + String.valueOf(filenum);
-            try {
-                ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(rfname));
-                while ((rightpage = right.next()) != null) {
-                    out.writeObject(rightpage);
-                }
-                out.close();
-            } catch (IOException io) {
-                System.out.println("NestedJoin: Error writing to temporary file");
-                return false;
-            }
-            if (!right.close())
-                return false;
+        	return true;
         }
-        if (left.open())
-            return true;
-        else
-            return false;
     }
 
-    /**
-     * from input buffers selects the tuples satisfying join condition
-     * * And returns a page of output tuples
-     **/
+    @Override
     public Batch next() {
-        int i, j;
-        if (eosl) {
-            return null;
-        }
-        outbatch = new Batch(batchsize);
-        while (!outbatch.isFull()) {
-            if (lcurs == 0 && eosr == true) {
-                /** new left page is to be fetched**/
-                leftbatch = (Batch) left.next();
-                if (leftbatch == null) {
-                    eosl = true;
-                    return outbatch;
-                }
-                /** Whenever a new left page came, we have to start the
-                 ** scanning of right table
-                 **/
-                try {
-                    in = new ObjectInputStream(new FileInputStream(rfname));
-                    eosr = false;
-                } catch (IOException io) {
-                    System.err.println("NestedJoin:error in reading the file");
-                    System.exit(1);
-                }
-
+    	Batch joinBatch = new Batch(batchNum);
+    	while (!joinBatch.isFull() && leftSort.peekTuple() != null && rightSort.peekTuple() != null) {
+    		Tuple leftTuple = leftSort.peekTuple();
+    		Tuple rightTuple = rightSort.peekTuple();
+    		int comparison = Tuple.compareTuples(leftTuple, rightTuple, leftJoinAttrIdx, rightJoinAttrIdx);
+            if (comparison < 0) {           
+                leftSort.nextTuple();
+            } else if (comparison > 0) {
+            	rightSort.nextTuple();
+            } else {  
+                Tuple joinTuple = leftTuple.joinWith(rightTuple);
+            	rightSort.nextTuple();
+                joinBatch.add(joinTuple);
             }
-            while (eosr == false) {
-                try {
-                    if (rcurs == 0 && lcurs == 0) {
-                        rightbatch = (Batch) in.readObject();
-                    }
-                    for (i = lcurs; i < leftbatch.size(); ++i) {
-                        for (j = rcurs; j < rightbatch.size(); ++j) {
-                            Tuple lefttuple = leftbatch.get(i);
-                            Tuple righttuple = rightbatch.get(j);
-                            if (lefttuple.checkJoin(righttuple, leftindex, rightindex)) {
-                                Tuple outtuple = lefttuple.joinWith(righttuple);
-                                outbatch.add(outtuple);
-                                if (outbatch.isFull()) {
-                                    if (i == leftbatch.size() - 1 && j == rightbatch.size() - 1) {  //case 1
-                                        lcurs = 0;
-                                        rcurs = 0;
-                                    } else if (i != leftbatch.size() - 1 && j == rightbatch.size() - 1) {  //case 2
-                                        lcurs = i + 1;
-                                        rcurs = 0;
-                                    } else if (i == leftbatch.size() - 1 && j != rightbatch.size() - 1) {  //case 3
-                                        lcurs = i;
-                                        rcurs = j + 1;
-                                    } else {
-                                        lcurs = i;
-                                        rcurs = j + 1;
-                                    }
-                                    return outbatch;
-                                }
-                            }
-                        }
-                        rcurs = 0;
-                    }
-                    lcurs = 0;
-                } catch (EOFException e) {
-                    try {
-                        in.close();
-                    } catch (IOException io) {
-                        System.out.println("NestedJoin: Error in reading temporary file");
-                    }
-                    eosr = true;
-                } catch (ClassNotFoundException c) {
-                    System.out.println("NestedJoin: Error in deserialising temporary file ");
-                    System.exit(1);
-                } catch (IOException io) {
-                    System.out.println("NestedJoin: Error in reading temporary file");
-                    System.exit(1);
-                }
-            }
-        }
-        return outbatch;
+    	}
+    	if (!joinBatch.isEmpty()) {
+    		return joinBatch;
+    	} else {
+    		return null;
+    	}
     }
-
-    /**
-     * Close the operator
-     */
+    
     public boolean close() {
-        File f = new File(rfname);
-        f.delete();
-        return true;
+    	return leftSort.close() && rightSort.close();
     }
-
 }
